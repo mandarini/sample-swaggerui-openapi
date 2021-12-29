0cae68a0acb2f379acb256d4df2c59f1366d2d51
Katerina Skroumpelou <sk.katherine@gmail.com>
2021-12-29 19:18:08 +0200

    combine files into single one

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import SwaggerUI from "swagger-ui";
 import "swagger-ui/dist/swagger-ui.css";
 
-const spec = require("./swagger-config.yaml");
+const spec = require("../output/swagger-config-merged.yaml");
 
 const ui = SwaggerUI({
   spec,
-  urls: [
-    { url: "./clients.yaml", name: "Clients" },
-    { url: "./users.yaml", name: "Users" },
-  ],
   deepLinking: true,
   dom_id: "#swagger",
 });
